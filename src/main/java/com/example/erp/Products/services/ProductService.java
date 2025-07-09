90fc9369f044d04ba135895de28d759c32abc851
pativatkongsawat <pativat.k@kkumail.com>
2025-07-09 13:19:54 +0700

    [ADD] Product Update Del

--- src/main/java/com/example/erp/Products/services/ProductService.java
+++ src/main/java/com/example/erp/Products/services/ProductService.java
@@ -16,29 +16,27 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository){
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public List<Product> GetAll(){
+    public List<Product> GetAll() {
         return productRepository.findAll();
     }
 
-    public Optional<Product> GetById(Integer id){
+    public Optional<Product> GetById(Integer id) {
 
         return productRepository.findById(id);
 
     }
 
-    public Product CreateProduct(CreateProduct data){
+    public Product CreateProduct(CreateProduct data) {
 
         boolean productNotNullAndNotBlank = data.getProductCode() != null && !data.getProductCode().isBlank() &&
-                                            data.getProductName() != null && !data.getProductName().isBlank() &&
-                                            data.getPrice() != null && data.getStock() != null;
+                data.getProductName() != null && !data.getProductName().isBlank() &&
+                data.getPrice() != null && data.getStock() != null;
 
-
-
-        if (!productNotNullAndNotBlank){
+        if (!productNotNullAndNotBlank) {
 
             throw new NoSuchElementException("All fields must have data");
 
@@ -52,17 +50,58 @@ public class ProductService {
         product.setProductName(data.getProductName());
         product.setPrice(data.getPrice());
         product.setStock(data.getStock());
-        
-
-
 
         product.setCreateAt(now);
 
-
         return productRepository.save(product);
 
-        
+    }
+
+    public Optional<Product> UpdateProduct(CreateProduct data, Integer id) {
+
+        boolean productNotNullAndNotBlank = data.getProductCode() == null || data.getProductCode().isBlank() &&
+                data.getProductName() == null || data.getProductName().isBlank() &&
+                        data.getPrice() == null
+                || data.getPrice() <= 0
+                        && data.getStock() == null
+                || data.getStock() <= 0;
+
+        if (productNotNullAndNotBlank) {
+
+            throw new NoSuchElementException("Add at least 1 field to update");
+
+        }
+
+        return productRepository.findById(id).map(product -> {
+
+            if (data.getProductCode() != null && !data.getProductCode().isBlank()) {
+                product.setProductCode(data.getProductCode());
+            }
+            if (data.getProductName() != null && !data.getProductName().isBlank()) {
+                product.setProductName(data.getProductName());
+            }
+            if (data.getPrice() != null && data.getPrice() > 0) {
+                product.setPrice(data.getPrice());
+            }
+            if (data.getStock() != null  && data.getStock() > 0) {
+                product.setStock(data.getStock());
+            }
+            return productRepository.save(product);
+
+        }).or(() ->{
+
+            throw new NoSuchElementException("Product ID Not Found");
+
+        });
 
     }
-    
+
+    public boolean DeleteProduct(Integer id ){
+        return productRepository.findById(id).map(data -> {
+            productRepository.delete(data);
+            return true;
+
+        }).orElseThrow(()-> new NoSuchElementException("Product ID Not Found"));
+    }
+
 }
