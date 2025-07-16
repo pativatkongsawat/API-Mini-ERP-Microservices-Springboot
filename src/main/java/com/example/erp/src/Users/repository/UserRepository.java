db73b71fd2381614de5e60fe3581c42c7725b200
pativatkongsawat <pativat.k@kkumail.com>
2025-07-16 18:51:08 +0700

    [EDIT] User Repository

--- src/main/java/com/example/erp/src/Users/repository/UserRepository.java
+++ src/main/java/com/example/erp/src/Users/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.erp.src.Users.repository;
 
 import java.util.Optional;
-import java.util.List;
+
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
