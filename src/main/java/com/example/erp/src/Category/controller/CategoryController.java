b35f2da5204fbb0f0787f7592fb6e9c626c1eea6
pativatkongsawat <pativat.k@kkumail.com>
2025-07-16 16:25:29 +0700

    [ADD] Controller Category

--- src/main/java/com/example/erp/src/Category/controller/CategoryController.java
+++ src/main/java/com/example/erp/src/Category/controller/CategoryController.java
@@ -2,9 +2,14 @@ package com.example.erp.src.Category.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.erp.Helper.ApiResponse;
@@ -22,13 +27,30 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @GetMapping("/")
     public List<Category> GetAll() {
         return categoryService.GetAll();
 
     }
 
+    @PostMapping("/")
     public ResponseEntity<ApiResponse<Category>> CreateCategory(@RequestBody CreateCategory data) {
 
+        Category category = categoryService.CreateCategory(data);
+        
+        ApiResponse<Category> res = new ApiResponse<>("201" , "Create Category Success" , category);
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<Void> DeleteCategory(@RequestParam Integer id){
+
+        categoryService.DeleteCategory(id);
+
+        return ResponseEntity.noContent().build();
 
     }
 }
